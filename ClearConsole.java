d8cf53f14270d45c7cf9434ddee4666e1ce8873c
Marcos Conde <155701351+MarcosCondeJr@users.noreply.github.com>
2024-08-31 01:28:14 -0300

    Add files via upload

---
+++ ClearConsole.java
@@ -0,0 +1,9 @@
+package construtor;
+
+public class ClearConsole {
+    public void limpar(){
+        for(int i = 0; i <= 20; i++){
+            System.out.println();
+        }
+    }
+}
---
+++ Inicio.java
@@ -0,0 +1,11 @@
+package construtor;
+
+public class Inicio {
+    public void telaInicio(){
+        System.out.println("------  BIBLIOTECA  -------\n");
+        System.out.println("1 - livros\n"
+                + "2 - Alugar\n"
+                + "3 - Devolução\n"
+                + "\nCondição: ");
+    }
+}
